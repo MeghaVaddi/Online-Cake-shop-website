ff98803e5e993445b7f3c029b2084b347e5d5f7a
MeghaVaddi <165870905+MeghaVaddi@users.noreply.github.com>
2024-06-11 19:44:47 +0530

    script.js

--- script.js
+++ script.js
@@ -13,6 +13,14 @@
 
                   let src = $(this).attr('data-src');
                   $('#cake-img').attr('src',src);
-            })
+            });
+            $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 0) {
+            $('header').addClass('scrolled');
+        } else {
+            $('header').removeClass('scrolled');
+        }
+    });
+            
 	});
 
